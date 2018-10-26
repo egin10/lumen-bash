d1845fded1fee85fdda37153ac681c0f28849039
egin10 <egin066@gmail.com>
2018-10-26 17:34:25 +0800

    add create controller, middleware and server command

---
+++ README.md
@@ -0,0 +1,18 @@
+## #Bash for Lumen
+
+create NewController and NewMiddleware using bash script and running lumen with simple way
+
+### how to set
+
+- copy file `lumen.sh` to your lumen root directory
+- create `lumen.sh` to executable, use `chmod u+x lumen.sh`
+
+### how to use
+
+- create new Controller : `./lumen.sh make:controller [newController]`
+- create new Middleware : `./lumen.sh make:middleware [newMiddleware]`
+- create new Middleware : `./lumen.sh [localhost:8000]`
+
+add new command is welcome 🔧 and send me PR
+
+egin10
--- lumen.sh
+++ lumen.sh
@@ -1,10 +1,46 @@
 #!/bin/bash
 
-if [ -z "$1" ] || [ $1 == "--help" ]
+if [ -z "$1" ] || [ $1 == "--help" ] || [ $1 == "help" ]
 then
     echo -e "Command List                                              | Descriptions"
-    echo -e "\e[32m./lumen.sh make:controller [controller_name]              \e[35mCreate new controller"
-elif [ $1 == 'yoo' ]
+    echo -e "\e[32m./lumen.sh make:controller [NameController]              \e[35mCreate new controller"
+    echo -e "\e[32m./lumen.sh make:controller [NameMiddleware]              \e[35mCreate new middleware"
+    echo -e "\e[32m./lumen.sh serve [localhost:8000]                        \e[35mStart Lumen Service"
+    echo -e "\e[32m./lumen.sh --help || help                                \e[35mShow help list"
+
+elif [ $1 == 'make:controller' ]
 then
-    echo "this is yoo"
+    if [ -z "$2" ]
+    then
+        echo "Please insert new controller name."
+    else
+        cd app/Http/Controllers
+        cp ExampleController.php $2.php
+        sed -i "s/ExampleController/$2/" $2.php
+        echo "$2 has created."
+    fi
+
+elif [ $1 == 'make:middleware' ]
+then
+    if [ -z "$2" ]
+    then
+        echo "Please insert new middleware name."
+    else
+        cd app/Http/Middleware
+        cp ExampleMiddleware.php $2.php
+        sed -i "s/ExampleMiddleware/$2/" $2.php
+        echo "$2 has created."
+    fi
+
+elif [ $1 == 'serve' ]
+then
+    if [ -z "$2" ]
+    then
+        echo "Please insert host:port"
+    else
+        php -S $2 -t public/
+    fi
+else
+    echo -e "\e[91mCommand not found!"
+    echo "--help for more command list"
 fi
